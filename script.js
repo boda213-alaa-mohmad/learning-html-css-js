7c12a437594680b9e8bc4deda5bcae1bd61ba444
boda213-alaa-mohmad <alaaabdelrhman213@gmail.com>
2025-03-11 09:40:52 +0200

    We add script.js style.css

--- script.js
+++ script.js
@@ -379,13 +379,12 @@ emember, every step you take—no matter how small—is bringing you closer to y
 
 
 
+function createArray(number) {
+    const newArray = [];
+    for (let counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+    return newArray;
+}
 
-
-
-
-
-
-
-
-
-
+console.log(createArray(4))
